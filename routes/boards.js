ab31cc2bc36398dff94eb0ea6036de763f2fc13b
JERSHA2712 <76258267+JERSHA20PW13@users.noreply.github.com>
2023-03-31 23:59:52 +0530

    get all boards done

--- routes/boards.js
+++ routes/boards.js
@@ -1,17 +1,70 @@
 const express = require("express");
 const router = express.Router();
+const multer = require("multer");
 
 const BoardModel = require("../models/BoardModel");
+const PinsModel = require("../models/PinsModel");
 
-router.post("/create-board", (req, res) => {
+router.post("/create-board", multer().none(), (req, res) => {
   const document = new BoardModel({
     userid: req.body.userid,
     boardid: req.body.boardid,
     title: req.body.title,
-    pinids: [req.body.firstpinid],
+    pinids: [],
   });
+  document.pinids.push(req.body.firstpinid);
   document.save();
   res.send("Board created");
+});
+
+router.put("/update-boards/:boardid", multer().none(), (req, res) => {
+  BoardModel.findOneAndUpdate(
+    { boardid: req.params.boardid },
+    {
+      $push: { pinids: req.body.newpinid },
+    }
+  )
+    .then((docs) => {
+      res.send(docs);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+});
+
+router.get("/get-boards/:userid", (req, res) => {
+  BoardModel.find({ userid: req.params.userid })
+    .then((docs) => {
+      console.log(docs);
+      const promises = docs.map((board) => {
+        const pins = [];
+        const promiseArray = [];
+        board.pinids.forEach((pinid) => {
+          const promise = PinsModel.find({ pinid: pinid })
+            .then((pin) => {
+              console.log(pin);
+              pins.push(pin);
+            })
+            .catch((err) => {
+              console.error(err);
+            });
+          promiseArray.push(promise);
+        });
+        return Promise.all(promiseArray).then(() => ({
+          userid: board.userid,
+          boardid: board.boardid,
+          title: board.title,
+          pinsArray: pins,
+        }));
+      });
+      Promise.all(promises).then((result) => {
+        console.log(result);
+        res.send(result);
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 });
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -34,13 +34,11 @@ router.put("/update-user/:userid", upload.single("profile"), (req, res) => {
       username: req.body.username,
       about: req.body.about,
       profile: req.profile,
-      ...(req.profile && {
-        profileUri:
-          "https://pinfinity.onrender.com/user/get-profile/" +
-          req.params.userid +
-          "/" +
-          Date.now(),
-      }),
+      profileUri:
+        "https://pinfinity.onrender.com/user/get-profile/" +
+        req.params.userid +
+        "/" +
+        Date.now(),
     }
   )
     .then((docs) => {
